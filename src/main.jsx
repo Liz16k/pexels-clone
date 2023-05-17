763436553e10de987a7356f95927663b88d2f7ba
Liz16k <prohoren4ik@gmail.com>
2023-05-17 22:30:53 +0300

    feat: connect local storage

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 import router from "./router";
-import store from "./store.js";
+import store from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,31 @@
 import { configureStore } from "@reduxjs/toolkit";
 import photosReducer from "./photosSlice";
 
+const loadFromLocalStorage = (key) => {
+  const serializedState = localStorage.getItem(key);
+  if (serializedState === null) return undefined;
+  return JSON.parse(serializedState);
+};
+
+const saveToLocalStorage = (key, state) => {
+  const serializedState = JSON.stringify(state);
+  localStorage.setItem(key, serializedState);
+};
+
+const localStorageMiddleware = (store) => (next) => (action) => {
+  const result = next(action);
+  saveToLocalStorage("reduxState", store.getState());
+  return result;
+};
+
+const persistedState = loadFromLocalStorage("reduxState");
+
 const store = configureStore({
   reducer: {
     photos: photosReducer,
   },
+  middleware: [localStorageMiddleware],
+  preloadedState: persistedState,
 });
 
 export default store;
